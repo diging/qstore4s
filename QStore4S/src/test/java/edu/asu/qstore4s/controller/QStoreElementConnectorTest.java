d9a26ce92839ecdf6e19ae10d37e9e4896e921a8
rsatyasi <rsatyasi@asu.edu>
2015-11-06 10:20:45 -0800

    Test classes for story/QSTR-101

--- QStore4S/src/test/java/edu/asu/qstore4s/controller/QStoreElementConnectorTest.java
+++ QStore4S/src/test/java/edu/asu/qstore4s/controller/QStoreElementConnectorTest.java
@@ -22,6 +22,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.data.neo4j.conversion.QueryResultBuilder;
 import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.data.neo4j.support.Neo4jTemplate;
 
@@ -36,29 +37,45 @@ import edu.asu.qstore4s.service.IStatistics;
 import edu.asu.qstore4s.service.impl.Statistics;
 import static org.junit.Assert.*;
 
+/**
+ * Test class to test the getElementCount() method of the ElementNeo4jConnector
+ * class
+ * 
+ * @author Rachita Satyasi
+ *
+ */
 @RunWith(MockitoJUnitRunner.class)
 public class QStoreElementConnectorTest {
-    
+
     @Mock
     private Neo4jTemplate template;
-    
+    @Mock
+    Result<Map<String, Object>> nodes;
+
     @InjectMocks
     private ElementNeo4jConnector conn;
-    
+
     @Before
-    public void setup(){
-        
-        template=Mockito.mock(Neo4jTemplate.class);
-        
+    public void setup() {
+
+        template = Mockito.mock(Neo4jTemplate.class);
+        nodes = Mockito.mock(Result.class);
+
         MockitoAnnotations.initMocks(this);
     }
-    
+
     @Test
-    public void testgetElementCount(){
+    public void testgetElementCount() {
         Map<String, Object> params = new HashMap<String, Object>();
-        Result<HashMap<String, Object>> nodes=(Result<HashMap<String, Object>>) new HashMap<String, Object>();
-        ((Map<String, Object>) nodes).put("nodes", 26);
-        //when(template.query("",params)).thenReturn(nodes);
+        when(template.query("match n return count(n)", params)).thenReturn(
+                nodes);
+        Map<String, Object> count = new HashMap<String, Object>();
+        count.put("count(n)", 26);
+        when(nodes.single()).thenReturn(count);
+        int nodecount = 0;
+        nodecount = conn.getElementCount();
+
+        assertEquals(count.get("count(n)"), nodecount);
     }
 
 }
