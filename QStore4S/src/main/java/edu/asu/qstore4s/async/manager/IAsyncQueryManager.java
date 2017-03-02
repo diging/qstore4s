36251d8ce4a7614342e76a780c8f2d72f8c05b96
Supraj Kolluri <supcena@gmail.com>
2017-03-02 09:39:22 -0700

    [QSTR-120] Adding Java doc

--- QStore4S/src/main/java/edu/asu/qstore4s/async/manager/IAsyncQueryManager.java
+++ QStore4S/src/main/java/edu/asu/qstore4s/async/manager/IAsyncQueryManager.java
@@ -2,12 +2,32 @@ package edu.asu.qstore4s.async.manager;
 
 public interface IAsyncQueryManager {
 
+    /**
+     * Tells if a particular query is executing or not
+     * @param queryID
+     * @return
+     */
     Boolean isQueryRunning(String queryID);
 
+    /**
+     * Set the status of a query
+     * @param queryID
+     * @param status
+     */
     void setQueryStatus(String queryID, boolean status);
 
+    /**
+     * Set the result of a query
+     * @param queryID
+     * @param res
+     */
     void setQueryResult(String queryID, String res);
 
+    /**
+     * Get the result of a query
+     * @param queryID
+     * @return
+     */
     String getQueryResult(String queryID);
 
 }
--- QStore4S/src/main/java/edu/asu/qstore4s/async/manager/impl/AsyncQueryManager.java
+++ QStore4S/src/main/java/edu/asu/qstore4s/async/manager/impl/AsyncQueryManager.java
@@ -8,17 +8,29 @@ import org.springframework.stereotype.Service;
 import edu.asu.qstore4s.async.QueryInformation;
 import edu.asu.qstore4s.async.manager.IAsyncQueryManager;
 
+/**
+ * 
+ * This class is used for handling asynchronous query executions
+ *
+ */
 @Service
 public class AsyncQueryManager implements IAsyncQueryManager {
 
+    // This map stores all the information about the query
     private Map<String, QueryInformation> queryStatusMap = new ConcurrentHashMap<>();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Boolean isQueryRunning(String queryID) {
         QueryInformation info = queryStatusMap.get(queryID);
         return info != null ? info.isQueryRunning() : false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setQueryStatus(String queryID, boolean status) {
         QueryInformation info = queryStatusMap.get(queryID);
@@ -30,12 +42,18 @@ public class AsyncQueryManager implements IAsyncQueryManager {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void setQueryResult(String queryID, String res) {
         QueryInformation info = queryStatusMap.get(queryID);
         info.setResult(res);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getQueryResult(String queryID) {
         QueryInformation info = queryStatusMap.get(queryID);
